9a3f8cb3e0256d43c902d9402684a0a222ad63da
Cristian1Alexar <cristian.archilac@gmail.com>
2020-09-07 19:56:17 -0600

    Se agregaron las tareas minifyJSTask e imagesTask, ademas se modificó la tarea scssTask para que realizara la minificación del archivo CSS resultante, además se agregó la tarea minifyTask a las tareas en serie realizadas por watchTask

--- gulpfile.js
+++ gulpfile.js
@@ -3,26 +3,45 @@ const {src, dest, series, parallel, watch} = require('gulp');
 //Paquetes 
 const sass = require('gulp-sass');
 const browserSync = require('browser-sync').create();
-
+const uglify = require('gulp-uglify');
+const minifycss =require('gulp-minify-css');
+const imagemin = require('gulp-imagemin');
 //Constantes de trabajo 
 
 const files = {
     scssPath: 'src/scss/**/*.scss',
-    htmlPath: 'dist/**/*.html'
+    htmlPath: 'dist/**/*.html',
+    jsPath: 'src/js/**/*.js', 
+    jsDistPath: 'dist/js/**/*.js',
+    imagesDistPath: 'dist/images/**/*',
+    imagePath: 'src/images/**/*'
 }
 
 //tareas 
 //compilar archivos de SCSS a CSS
 function scssTask (done){
     return src(files.scssPath)
-    .pipe(sass())
+    .pipe(sass())//pasar archivos SCSS a CSS
+    .pipe(minifycss())//minificar CSS producido por sass()
     .pipe(dest('dist/css')); 
 }
+
+function minifyJSTask(done){
+    return src(files.jsPath)
+    .pipe(uglify())//minificar archivos js
+    .pipe(dest('dist/js')); 
+}
+function imagesTask(done){
+    return src(files.imagePath)
+    .pipe(imagemin())
+    .pipe(dest('dist/images'));
+    }
+
 //observador 
 function watchTask(){
     watch(
-        [files.scssPath, files.htmlPath],
-        series(scssTask, reloadTask)
+        [files.scssPath, files.htmlPath, files.jsPath, files.imagesDistPath],
+        series(scssTask, minifyJSTask, reloadTask)
         )
 }
 
@@ -40,9 +59,11 @@ function reloadTask(done){
     done()
 }
 
-//exports.css = scssTask;  
 
+//exports.css = scssTask;  
+exports.images = imagesTask;
 exports.default = series(scssTask, serveTask, watchTask);
+
 
 
 
